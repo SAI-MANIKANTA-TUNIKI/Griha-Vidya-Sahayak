f444e806fed59540aa704845000ffd976b78808d
SAI-MANIKANTA-TUNIKI <saimanikantatuniki@gmail.com>
2025-04-21 00:59:28 +0530

    packages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Griha-Vidya Sahayak/",
+  base: '/Griha-Vidya-Sahayak/'
 })
